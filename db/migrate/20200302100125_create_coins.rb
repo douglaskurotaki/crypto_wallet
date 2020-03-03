79f1b4fa42d079e7dd33c1bd6ccad8d83e34996d
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-03-02 22:21:20 -0300

    Create controller and view welcome index

--- db/migrate/20200302100125_create_coins.rb
+++ db/migrate/20200302100125_create_coins.rb
@@ -2,7 +2,7 @@ class CreateCoins < ActiveRecord::Migration[5.2]
   def change
     create_table :coins do |t|
       t.string :description
-      t.String :acronym
+      t.string :acronym
       t.string :url_image
 
       t.timestamps
