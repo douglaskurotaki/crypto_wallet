8f66069e6a3e7e88a0df3432860a3a155a088796
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-03-03 21:24:03 -0300

    create helper for time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# For all uses
 module ApplicationHelper
+  def data_br(data_us)
+    data_us.strftime('%d/%m/%Y')
+  end
+
+  def nome_aplicacao
+    'CRYPTO WALLET'
+  end
 end
